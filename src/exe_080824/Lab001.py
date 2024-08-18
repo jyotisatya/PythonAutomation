313dcaf2acac551768f7d24def66bafa453f050e
jyoti <jyoti1.satya@gmail.com>
2024-08-18 13:09:18 +0530

    Have added the lab exercises for 13 aug

---
+++ src/exe_080824/Lab001.py
@@ -0,0 +1,6 @@
+a= int(input('Enter a no '))
+i = 1
+while i <= 10:
+    result = a * i
+    print(f"{a} * {i} = {result}")
+    i= i+1
---
+++ src/exe_080824/Lab002.py
@@ -0,0 +1,9 @@
+
+num1 =int(input('Enter number1 '))
+num2 =int(input('Enter number2'))
+sum = num1 + num2
+print(f"sum of {num1} and {num2} is {sum}")
+mul = num1 * num2
+print(f"Multiplication of {num1} and {num2} is {mul}")
+power = pow(num1 ,num2)
+print(f"power of {num1} and {num2} is {power}")
